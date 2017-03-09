850c6367249531745a06b3600f26c0e59d675f1b
Derek Stucki <derek.stucki@inl.gov>
2017-03-09 09:39:26 -0700

    changed background to inl logo

--- background_gl.c
+++ background_gl.c
@@ -136,9 +136,9 @@ void create_background_texture(background_t *state)
     unsigned width, height;
 
     #ifdef RASPI
-    error = lodepng_decode32_file(&image, &width, &height, "SPH/images/OakRidgeLeaf.png");
+    error = lodepng_decode32_file(&image, &width, &height, "SPH/images/inl_logo.png");
     #else
-    error = lodepng_decode32_file(&image, &width, &height, "images/OakRidgeLeaf.png");
+    error = lodepng_decode32_file(&image, &width, &height, "images/inl_logo.png");
     #endif
     if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
 
--- egl_utils.c
+++ egl_utils.c
@@ -176,7 +176,7 @@ void init_ogl(gl_t *state, render_t *render_state)
 
     // Open input event
     state->controller_1_fd = open("/dev/input/event0",O_RDONLY|O_NONBLOCK);
-    state->controller_2_fd = open("/dev/input/event1",O_RDONLY|O_NONBLOCK);
+    state->controller_2_fd = open("/dev/input/event3",O_RDONLY|O_NONBLOCK);
 
     #ifdef LEAP_MOTION_ENABLED
       curl_chunk.memory = malloc(1);
